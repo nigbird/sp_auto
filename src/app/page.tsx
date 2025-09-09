7c1879c70990becd029e4c70f2c76f4d86a2a733
Haymi <hayminib@gmail.com>
2025-09-09 08:15:01 +0000

    Add a summary section to the dashboard at the top of the page that displ

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,18 @@
 
 import { getReportData, getActivities } from "@/lib/data";
 import { DepartmentalDashboard } from "@/components/dashboard/departmental-dashboard";
+import { ReportSummaryCards } from "@/components/reports/summary-cards";
+import { generateReportSummary } from "@/lib/utils";
 
 export default async function DashboardPage() {
   const reportData = await getReportData();
   const activities = await getActivities();
   const departments = ["All", ...new Set(activities.map((a) => a.department))];
+  const summary = generateReportSummary(reportData);
 
   return (
     <div className="flex-1 space-y-6">
+      <ReportSummaryCards summary={summary} />
       <DepartmentalDashboard 
         activities={activities}
         departments={departments}
