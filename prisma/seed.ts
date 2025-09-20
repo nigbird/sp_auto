011bc18040efd66125187014aea5bae508cbf688
Nuh Mh <shifteher@gmail.com>
2025-09-20 07:41:23 +0000

    Provide a prisma/seed.ts file with initial seed data to populate the dat

---
+++ prisma/seed.ts
@@ -0,0 +1,220 @@
+
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+
+// Data from src/lib/data.ts (adapted for seeding)
+const users = [
+  { name: "Liam Johnson", email: "liam@corp-plan.com", avatar: "https://picsum.photos/id/1005/100", role: "Manager", status: "Active", createdAt: new Date("2023-10-01") },
+  { name: "Olivia Smith", email: "olivia@corp-plan.com", avatar: "https://picsum.photos/id/1011/100", role: "User", status: "Active", createdAt: new Date("2023-10-05") },
+  { name: "Noah Williams", email: "noah@corp-plan.com", avatar: "https://picsum.photos/id/1012/100", role: "User", status: "Active", createdAt: new Date("2023-10-12") },
+  { name: "Emma Brown", email: "emma@corp-plan.com", avatar: "https://picsum.photos/id/1013/100", role: "User", status: "Active", createdAt: new Date("2023-10-15") },
+  { name: "Oliver Jones", email: "oliver@corp-plan.com", avatar: "https://picsum.photos/id/1014/100", role: "User", status: "Inactive", createdAt: new Date("2023-11-01") },
+  { name: "Admin User", email: "admin@corp-plan.com", avatar: "https://picsum.photos/id/1/100", role: "Administrator", status: "Active", createdAt: new Date("2023-09-01") },
+];
+
+const rules = [
+  { id: "1", status: "Completed As Per Target", description: "Performance against target is >= 100%", min: 100, max: Infinity, isSystem: true },
+  { id: "2", status: "On Track", description: "Performance against target is from 70% up to 99.99%", min: 70, max: 99.99, isSystem: false },
+  { id: "3", status: "Delayed", description: "Performance against target is above 0% but less than 70%", min: 0.01, max: 69.99, isSystem: false },
+  { id: "4", status: "Not Started", description: "Performance against target is 0%", min: 0, max: 0, isSystem: true },
+  { id: "5", status: "Overdue", description: "Past deadline and not completed", min: 0, max: 99.99, isSystem: true },
+];
+
+const notifications = [
+    { id: "1", message: "Activity 'Customer Support Training' is delayed.", date: new Date(), read: false },
+    { id: "2", message: "Noah Williams completed 'Website Redesign Project'.", date: new Date(Date.now() - 1000 * 60 * 60 * 2), read: false },
+    { id: "3", message: "Deadline for 'New CRM System Implementation' is approaching.", date: new Date(Date.now() - 1000 * 60 * 60 * 24), read: true },
+];
+
+
+async function main() {
+    console.log(`Start seeding ...`);
+
+    for (const u of users) {
+        const user = await prisma.user.upsert({
+            where: { email: u.email },
+            update: u,
+            create: u,
+        });
+        console.log(`Created user with id: ${user.id}`);
+    }
+    
+    for (const r of rules) {
+        await prisma.rule.upsert({
+            where: { id: r.id },
+            update: {
+                status: r.status,
+                description: r.description,
+                min: r.min,
+                max: r.max,
+                isSystem: r.isSystem,
+            },
+            create: {
+                id: r.id,
+                status: r.status,
+                description: r.description,
+                min: r.min,
+                max: r.max,
+                isSystem: r.isSystem,
+            },
+        });
+    }
+    console.log(`Seeded ${rules.length} rules.`);
+    
+    for (const n of notifications) {
+         await prisma.notification.upsert({
+            where: { id: n.id },
+            update: {},
+            create: {
+                id: n.id,
+                message: n.message,
+                date: n.date,
+                read: n.read
+            }
+        });
+    }
+    console.log(`Seeded ${notifications.length} notifications.`);
+
+    const planData = {
+        planTitle: 'Corporate Strategic Plan',
+        startYear: 2024,
+        endYear: 2028,
+        version: "1.0",
+        status: 'published'
+    };
+
+    const strategicPlan = await prisma.strategicPlan.create({
+        data: planData,
+    });
+    console.log(`Created strategic plan "${strategicPlan.planTitle}"`);
+
+    const pillar1 = await prisma.pillar.create({
+        data: {
+            title: "Market Leadership",
+            planId: strategicPlan.id,
+            objectives: {
+                create: [
+                    {
+                        statement: "Increase Market Share",
+                        initiatives: {
+                            create: {
+                                title: "Aggressive Marketing & Sales",
+                                activities: {
+                                    create: [
+                                        {
+                                            title: "Q3 Marketing Campaign Launch",
+                                            description: "Launch the new marketing campaign for the fall season, including social media, email, and content marketing.",
+                                            department: "Marketing",
+                                            responsible: "Olivia Smith",
+                                            startDate: new Date("2024-07-01"),
+                                            endDate: new Date("2024-09-30"),
+                                            status: "Delayed",
+                                            weight: 40,
+                                            progress: 56,
+                                            approvalStatus: 'Pending',
+                                        }
+                                    ]
+                                }
+                            }
+                        }
+                    },
+                    {
+                        statement: "Enhance Brand Presence",
+                        initiatives: {
+                            create: {
+                                title: "Digital Presence Overhaul",
+                                activities: {
+                                    create: {
+                                        title: "Website Redesign Project",
+                                        description: "Complete redesign of the corporate website for improved user experience and mobile responsiveness.",
+                                        department: "Engineering",
+                                        responsible: "Noah Williams",
+                                        startDate: new Date("2024-05-01"),
+                                        endDate: new Date("2024-07-31"),
+                                        status: "Completed As Per Target",
+                                        weight: 30,
+                                        progress: 100,
+                                        approvalStatus: 'Approved'
+                                    }
+                                }
+                            }
+                        }
+                    },
+                ]
+            }
+        }
+    });
+    console.log(`Seeded pillar: ${pillar1.title}`);
+    
+    const pillar2 = await prisma.pillar.create({
+        data: {
+            title: "Operational Excellence",
+            planId: strategicPlan.id,
+            objectives: {
+                create: [
+                    {
+                        statement: "Improve Employee Satisfaction",
+                        initiatives: {
+                            create: {
+                                title: "Workplace Wellness",
+                                activities: {
+                                    create: {
+                                        title: "Employee Wellness Program",
+                                        description: "Develop and roll out a new employee wellness program.",
+                                        department: "Human Resources",
+                                        responsible: "Emma Brown",
+                                        startDate: new Date("2024-08-01"),
+                                        endDate: new Date("2024-10-31"),
+                                        status: "Not Started",
+                                        weight: 50,
+                                        progress: 0,
+                                        approvalStatus: 'Pending'
+                                    }
+                                }
+                            }
+                        }
+                    },
+                    {
+                        statement: "Streamline Customer Service",
+                        initiatives: {
+                            create: {
+                                title: "Support Enhancement",
+                                activities: {
+                                    create: {
+                                        title: "Customer Support Training",
+                                        description: "Advanced training for all customer support staff on new product features.",
+                                        department: "Support",
+                                        responsible: "Oliver Jones",
+                                        startDate: new Date("2024-07-10"),
+                                        endDate: new Date("2024-07-20"),
+                                        status: "Delayed",
+                                        weight: 50,
+                                        progress: 50,
+                                        approvalStatus: 'Declined',
+                                        declineReason: "The delay reason is not sufficient. Please provide a more detailed recovery plan."
+                                    }
+                                }
+                            }
+                        }
+                    }
+                ]
+            }
+        }
+    });
+     console.log(`Seeded pillar: ${pillar2.title}`);
+
+    console.log(`Seeding finished.`);
+}
+
+main()
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
+
+    
