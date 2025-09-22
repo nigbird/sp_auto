59e4fa8474e77b91420a489469a0c575c23e0305
blen Nib <blennib12@gmail.com>
2025-09-22 08:23:51 +0000

    remove these two from the reports page

--- src/app/reports/page.tsx
+++ src/app/reports/page.tsx
@@ -11,24 +11,6 @@ export default async function ReportsPage() {
       <div className="flex items-center justify-between space-y-2">
         <h1 className="text-3xl font-bold tracking-tight">Reports</h1>
       </div>
-       <div className="grid gap-6 md:grid-cols-2 lg:grid-cols-7">
-        <Card className="lg:col-span-4">
-          <CardHeader>
-            <CardTitle>Pillar Progress</CardTitle>
-          </CardHeader>
-          <CardContent className="pl-2">
-            <p>Charts will be added here soon.</p>
-          </CardContent>
-        </Card>
-        <Card className="lg:col-span-3">
-          <CardHeader>
-            <CardTitle>KPI Performance</CardTitle>
-          </CardHeader>
-          <CardContent className="pl-2">
-            <p>Visualizations will be added here soon.</p>
-          </CardContent>
-        </Card>
-      </div>
       <ReportTable data={reportData} />
     </div>
   );
